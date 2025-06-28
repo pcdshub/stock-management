13a05dc22e0611bacbd8e24fb5c41b63a28a3bd7
CalciumKing <lkingerslev@gmail.com>
2025-06-27 17:31:39 -0700

    6/27 Friday Commit 2
    - Added a plethora of random testing files that I've been using the last couple of days to git just in case something happens to my computer

---
+++ stock_manager/tests/test_google_sheets_integration.py
@@ -0,0 +1,44 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def main():
+	scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+	
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('../assets/gs_credentials.json')
+	client = gspread.authorize(credentials)
+	# create_sheet(client)
+	# update_sheet(client)
+	read_sheet(client)
+
+
+def update_sheet(client):
+	sheet = client.open('Stock Management Sheet').sheet1
+	
+	sheet.update_cell(1, 1, 'Another Updated Value')
+
+
+# sheet.update(range_name='B1:C2', values=[['New Value 1', 'New Value 2'], ['New Value 3', 'New Value 4']])
+# sheet.batch_update([{
+# 	'range': 'D1',
+# 	'values': [['Batch Updated Value']]
+# }, {
+# 	'range': 'E1:F2',
+# 	'values': [['Batch', 'Update'], ['Example', 'Data']]
+# }])
+
+
+def read_sheet(client):
+	sheet = client.open('Stock Management Sheet').sheet1
+	print(sheet.get_all_records())
+	print(sheet.row_values(1))
+	print(sheet.cell(1, 1).value)
+
+
+def create_sheet(client):
+	sheet = client.create('Stock Management Sheet')
+	sheet.share('lkingerslev@gmail.com', 'user', 'writer')
+
+
+if __name__ == '__main__':
+	main()
