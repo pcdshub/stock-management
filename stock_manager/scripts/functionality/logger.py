10afa76923e35f61d76e27478e7dad4530f742ff
CalciumKing <lkingerslev@gmail.com>
2025-07-23 11:39:37 -0700

    Requested PR #2 Changes - Commit 8
    - Changed 'test' folder to 'scripts' because it only tests standalone features and has nothing to do with unit testing.
    
    - Created the basics of a 'tests' folder.
    
    - This is a strictly organizational commit

---
+++ stock_manager/scripts/functionality/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+if __name__ == '__main__':
+    logging.basicConfig(
+            filename='../assets/test.log',
+            level=logging.INFO,
+            format='%(asctime)s [%(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info("App started")
+    logging.warning("Low stock for item ID=5")
+    logging.error("Failed to connect to database")
