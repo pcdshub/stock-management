e582eaba157b553cf5679a9bff7b47a064406db8
CalciumKing <lkingerslev@gmail.com>
2025-07-29 12:45:08 -0700

    CLI Backend Changes
    - Changed CLI building to be more efficient and less confusing for anyone viewing code.
    
    - Moved all if/elif code to their own _run_... functions.
    
    - Added .validate_args() to check if the number of arguments is correct. Checking if arguments are valid using .validate_args() and possibly returning False is the first line of all _run_... functions.
    
    - Moved all commands to a command dictionary mapping instead of a long if-elif-else statement.
    
    - Moved all argument builds to an array of tuples to hold the short keyword, long keyword, help description. This system only uses one parser.add_argument() command since they are all using action='store_true'.
    
    - Overall, efficiency and line count changes, no frontend or user changes at all.

--- stock_manager/cli.py
+++ stock_manager/cli.py
@@ -1,88 +1,43 @@
 import argparse
+import os
+import sys
+from typing import Callable
+
+import pytest
 
 import stock_manager
+from stock_manager import DatabaseUpdateType, DBUtils, ExportUtils, Item
 
 
 def build_commands() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Common Stock Manager CLI')
     
-    # core arguments
-    parser.add_argument(
-            '-r', '--run',
-            action='store_true',
-            help='Runs Application GUI, Same As Running "python -m stock_manager"'
-    )
-    parser.add_argument(
-            '-v', '--version',
-            action='store_true',
-            help='Prints stock_manager\'s git version'
-    )
-    parser.add_argument(
-            '-t', '--test',
-            action='store_true',
-            help='Runs All Tests Located In stock_manager/tests Using PyTest'
-    )
-    parser.add_argument(
-            '-li', '--list-items',
-            action='store_true',
-            help='Lists All Items In The Database'
-    )
-    parser.add_argument(
-            '-lu', '--list-users',
-            action='store_true',
-            help='Lists All Users In The Database'
-    )
-    parser.add_argument(
-            '-e', '--export',
-            action='store_true',
-            help='Exports All Item Data To A Specified File Type And Location '
-                 '(Used With Path And Extension Arguments)'
-    )
-    parser.add_argument(
-            '-qr', '--create-qr',
-            action='store_true',
-            help='Generates A QR Code Of A Specified Item And Stores It As A '
-                 '.PNG In A Specified Location (Used With Path And Part Number Arguments)'
-    )
-    parser.add_argument(
-            '-si', '--search-items',
-            action='store_true',
-            help='Searches For A Specified Value In All Fields In All Items'
-    )
-    parser.add_argument(
-            '-su', '--search-users',
-            action='store_true',
-            help='Searches For A Specified Username In All Usernames'
-    )
-    parser.add_argument(
-            '-ai', '--add-item',
-            action='store_true',
-            help='Add An Item With Specified Values To Both Databases '
-                 '(Used With Sequence Of Item Detail Arguments)'
-    )
-    parser.add_argument(
-            '-au', '--add-user',
-            action='store_true',
-            help='Add A User With Specified Username To Both Databases'
-    )
-    parser.add_argument(
-            '-ri', '--remove-item',
-            action='store_true',
-            help='Remove A Specified Item From Both Databases After Confirmation Query'
-    )
-    parser.add_argument(
-            '-ru', '--remove-user',
-            action='store_true',
-            help='Remove A Specified User From Both Databases After Confirmation Query'
-    )
-    parser.add_argument(
-            '-s', '--sync-databases',
-            action='store_true',
-            help='Synchronize Both Databases, Setting The Values Of The SQL Database '
-                 'To That Of The Google Sheet (Only Run If A Database Is Changed Externally)'
-    )
+    flags = [
+        ('-r', '--run', 'Runs Application GUI, Same As Running "python -m stock_manager"'),
+        ('-v', '--version', 'Prints stock_manager\'s git version'),
+        ('-t', '--test', r'Runs All Tests Located In stock_manager\tests Using PyTest'),
+        ('-e', '--export', 'Exports All Item Data To A Specified File Type And Location '
+                           '(Used With Path And Extension Arguments)'),
+        ('-qr', '--create-qr', 'Generates A QR Code Of A Specified Item And Stores It As A '
+                               '.PNG In A Specified Location (Used With Path And Part Number Arguments)'),
+        ('-s', '--sync-databases', 'Synchronize Both Databases, Setting The Values Of The SQL Database '
+                                   'To That Of The Google Sheet (Only Run If A Database Is Changed Externally)'),
+        ('-li', '--list-items', 'Lists All Items In The Database'),
+        ('-si', '--search-items', 'Searches For A Specified Value In All Fields In All Items'),
+        ('-ai', '--add-item', 'Add An Item With Specified Values To Both Databases '
+                              '(Used With Sequence Of 10 Item Detail Arguments)'),
+        ('-ri', '--remove-item', 'Remove A Specified Item From Both Databases After Confirmation Query'),
+        ('-lu', '--list-users', 'Lists All Users In The Database'),
+        ('-su', '--search-users', 'Searches For A Specified Username In All Usernames'),
+        ('-au', '--add-user', 'Add A User With Specified Username To Both Databases'),
+        ('-ru', '--remove-user', 'Remove A Specified User From Both Databases After Confirmation Query'),
+    ]
     
-    # optional positional argument
+    # core arguments
+    for short, long, desc in flags:
+        parser.add_argument(short, long, action='store_true', help=desc)
+    
+    # optional positional arguments
     parser.add_argument(
             'values',
             nargs='*',
@@ -94,228 +49,219 @@ def build_commands() -> argparse.Namespace:
 
 
 def entry_point(args) -> bool:
-    if args.run:
-        if args.values:
-            print('[!] No Values Expected In Run Command, Got:', args.values)
-            return False
-        print('[+] Starting Application...')
-        return True
-    elif args.version:
-        if args.values:
-            print('[!] No Values Expected In Version Command, Got:', args.values)
-            return False
-        from stock_manager import __version__
-        print('[*] Version:', __version__)
-        return False
-    elif args.test:
-        if args.values:
-            print('[!] No Values Expected In Test Command, Got:', args.values)
-            return False
+    command_map: dict[str, Callable[[args], bool]] = {
+        'run': _run_app,
+        'version': _run_version,
+        'test': _run_tests,
+        'export': _run_export,
+        'create_qr': _run_qr,
+        'sync_databases': _run_sync_databases,
         
-        import pytest
-        import sys
-        import os
+        'list_items': _run_list_items,
+        'search_items': _run_search_items,
+        'add_item': _run_add_item,
+        'remove_item': _run_remove_item,
         
-        print('[+] Starting Tests...')
-        tests_path = os.path.join(os.path.dirname(__file__), 'tests')
-        return_code = pytest.main([tests_path])
-        sys.exit(return_code)
-    elif args.list_items:
-        if args.values:
-            print('[!] No Values Expected In List Items Command, Got:', args.values)
-            return False
-        _run_list_items()
-        return False
-    elif args.list_users:
-        if args.values:
-            print('[!] No Values Expected In List Users Command, Got:', args.values)
-            return False
-        _run_list_users()
-        return False
-    elif args.export:
-        if len(args.values) < 2:
-            print('[!] Please Enter A Path And Extension When Using Export Command')
-            return False
-        elif len(args.values) > 2:
-            print('[!] 2 Values Expected In Export Command, Got:', args.values[2:])
-            return False
-        
-        from stock_manager import ExportUtils, DBUtils
-        
-        path = args.values[0]
-        extension = args.values[1]
-        print(f'[+] Exporting Data As .{extension} File...')
-        
-        utils = ExportUtils()
-        db = DBUtils()
-        all_items = db.create_all_items(db.get_all_data_gs())
-        
-        match extension:
-            case 'csv' | 'psv' | 'tsv':
-                utils.sv_export(extension, path, all_items)
-            case 'pdf':
-                utils.pdf_export()
-            case _:
-                print('[!] Unknown Export Type:', extension)
-                return False
-        
-        print('[*] Successfully Exported Data To', extension, 'File')
-        return False
-    elif args.create_qr:
-        if len(args.values) < 2:
-            print('[!] Please Enter A Path and Part Number When Using QR Command')
-            return False
-        elif len(args.values) > 2:
-            print('[!] 2 Values Expected In Create QR Command, Got:', args.values[2:])
-            return False
-        
-        from stock_manager import ExportUtils, DBUtils
-        
-        path = args.values[0]
-        part_num = args.values[1]
-        print('[+] Exporting', part_num, 'QR Code As .PNG...')
-        utils = ExportUtils()
-        
-        image = utils.create_code(part_num)
-        if not image:
-            return False
-        
-        if not utils.save_code(image, path):
-            return False
-        
-        print('[*] Successfully Exported', part_num, ' QR Code')
-        return False
-    elif args.search_items:
-        if not args.values:
-            print(
-                    '[!] Please Enter A Value To Search For '
-                    'When Using Search Items Command'
-            )
-            return False
-        elif len(args.values) > 1:
-            print('[!] 1 Value Expected In Search Items Command, Got:', args.values[1:])
-            return False
-        _run_list_items(args.values[0])
-        return False
-    elif args.search_users:
-        if not args.values:
-            print(
-                    '[!] Please Enter A Username To Search For '
-                    'When Using Search Users Command'
-            )
-            return False
-        elif len(args.values) > 1:
-            print('[!] 1 Value Expected In Search Users Command, Got:', args.values[1:])
-            return False
-        _run_list_users(args.values[0])
-        return False
-    elif args.add_item:
-        from stock_manager import DBUtils, Item, DatabaseUpdateType
-        
-        if len(args.values) < 10:
-            print(
-                    '[!] Please Enter A Sequence Of 10 Item Values '
-                    'To Add To Both Databases When Using Add Item Command'
-            )
-            return False
-        elif len(args.values) > 10:
-            print('[!] 10 Values Expected In Add Item Command, Got:', args.values[10:])
-            return False
-        
-        print('[+] Adding Item To Databases...')
-        
-        try:
-            vals: list[str | int] = [
-                value if i not in [3, 4, 5, 6, 7, 8]
-                else int(value)
-                for i, value in enumerate(args.values)
-            ]
-            item = Item(*vals)
-            
-            DBUtils().update_items_database(DatabaseUpdateType.ADD, item)
-            
-            print('[*] Successfully Added', item.part_num, 'To Databases')
-        except ValueError as e:
-            print('[x] Failed To Add Item To Database:', e)
-        
-        return False
-    elif args.add_user:
-        from stock_manager import DBUtils, DatabaseUpdateType
-        
-        if not args.values:
-            print(
-                    '[!] Please Enter A Username To Add To Databases '
-                    'When Using Add User Command'
-            )
-            return False
-        elif len(args.values) > 1:
-            print('[!] 1 Value Expected In Add User Command, Got', args.values[1:])
-            return False
-        
-        print('[+] Adding User To Databases...')
-        
-        DBUtils().update_users_database(DatabaseUpdateType.ADD, args.values[0])
-        
-        print('[*] Successfully Added', args.values[0], 'To Databases')
-        return False
-    elif args.remove_item:
-        from stock_manager import DBUtils, DatabaseUpdateType
-        
-        if not args.values:
-            print(
-                    '[!] Please Enter A Part Number To Remove From Databases '
-                    'When Using Remove Item Command'
-            )
-            return False
-        elif len(args.values) > 1:
-            print('[!] 1 Value Expected In Remove Item Command, Got', args.values[1:])
-            return False
-        
-        print(f'[+] Removing "{args.values[0]}" From Databases...')
-        
-        utils = DBUtils()
-        item = utils.find_item(args.values[0])
-        if not item:
-            print(f'[x] Could Not Locate "{args.values[0]}" In Databases')
-            return False
-        
-        utils.update_items_database(DatabaseUpdateType.REMOVE, item)
-        
-        print('[*] Successfully Removed', item.part_num, 'From Databases')
-        return False
-    elif args.remove_user:
-        from stock_manager import DBUtils, DatabaseUpdateType
-        
-        if not args.values:
-            print(
-                    '[!] Please Enter A Username To Remove From Databases '
-                    'When Using Remove User Command'
-            )
-            return False
-        elif len(args.values) > 1:
-            print('[!] 1 Value Expected In Remove User Command, Got', args.values[1:])
-            return False
-        
-        print('[+] Removing User From Databases...')
-        
-        DBUtils().update_users_database(DatabaseUpdateType.REMOVE, args.values[0])
-        
-        print('[*] Successfully Removed', args.values[0], 'From Databases')
-        return False
-    elif args.sync_databases:
-        if args.values:
-            print('[!] No Values Expected In Sync Databases Command, Got:', args.values)
-            return False
-        print('[+] Syncing Databases...')
-        stock_manager.DBUtils().sync_databases()
-        print('[*] Successfully Synchronized Databases')
-        return False
-    else:
-        print('[+] Starting Application UI...')
-        return True
+        'list_users': _run_list_users,
+        'search_users': _run_search_user,
+        'add_user': _run_add_user,
+        'remove_user': _run_remove_user,
+    }
+    
+    for cmd, handler in command_map.items():
+        if getattr(args, cmd, False):
+            return handler(args)
+    
+    print('[+] Starting Application UI...')
+    return True
 
 
-def _run_list_items(search_value: str = '') -> None:
+def validate_args(args, expected=0) -> bool:
+    values = args.values or []
+    if expected:
+        if len(values) < expected or len(values) > expected:
+            print(f'[!] Expected {expected} values, got {len(values)}: {values}')
+            return False
+    elif values:
+        print(f'[!] No Values Expected, Got {len(values)}: {values}')
+        return False
+    return True
+
+
+def _run_app(args) -> bool:
+    if not validate_args(args):
+        return False
+    print('[+] Starting Application...')
+    return True
+
+
+def _run_version(args) -> bool:
+    if not validate_args(args):
+        return False
+    print('[*] Version:', stock_manager.__version__)
+    return False
+
+
+def _run_tests(args) -> bool:
+    if not validate_args(args):
+        return False
+    
+    print('[+] Starting Tests...')
+    
+    tests_path = os.path.join(os.path.dirname(__file__), 'tests')
+    return_code = pytest.main([tests_path])
+    
+    sys.exit(return_code)
+
+
+def _run_export(args) -> bool:
+    if not validate_args(args, 2):
+        return False
+    
+    path = args.values[0]
+    extension = args.values[1]
+    print(f'[+] Exporting Data As .{extension} File...')
+    
+    utils = ExportUtils()
+    db = DBUtils()
+    all_items = db.create_all_items(db.get_all_data_gs())
+    
+    match extension:
+        case 'csv' | 'psv' | 'tsv':
+            utils.sv_export(extension, path, all_items)
+        case 'pdf':
+            utils.pdf_export()
+        case _:
+            print('[!] Unknown Export Type:', extension)
+            return False
+    
+    print('[*] Successfully Exported Data To', extension, 'File')
+    return False
+
+
+def _run_qr(args) -> bool:
+    if not validate_args(args, 2):
+        return False
+    
+    path = args.values[0]
+    part_num = args.values[1]
+    print('[+] Exporting', part_num, 'QR Code As .PNG...')
+    utils = ExportUtils()
+    
+    image = utils.create_code(part_num)
+    if not image:
+        return False
+    
+    if not utils.save_code(image, path):
+        return False
+    
+    print('[*] Successfully Exported', part_num, ' QR Code')
+    return False
+
+
+def _run_sync_databases(args) -> bool:
+    if not validate_args(args):
+        return False
+    print('[+] Syncing Databases...')
+    stock_manager.DBUtils().sync_databases()
+    print('[*] Successfully Synchronized Databases')
+    return False
+
+
+def _run_list_items(args) -> bool:
+    if not validate_args(args):
+        return False
+    _list_items()
+    return False
+
+
+def _run_search_items(args) -> bool:
+    if not validate_args(args, 1):
+        return False
+    _list_items(args.values[0])
+    return False
+
+
+def _run_add_item(args) -> bool:
+    if not validate_args(args, 10):
+        return False
+    
+    print('[+] Adding Item To Databases...')
+    
+    try:
+        vals: list[str | int] = [
+            value if i not in [3, 4, 5, 6, 7, 8]
+            else int(value)
+            for i, value in enumerate(args.values)
+        ]
+        item = Item(*vals)
+        
+        DBUtils().update_items_database(DatabaseUpdateType.ADD, item)
+        
+        print('[*] Successfully Added', item.part_num, 'To Databases')
+    except ValueError as e:
+        print('[x] Failed To Add Item To Database:', e)
+    
+    return False
+
+
+def _run_remove_item(args) -> bool:
+    if not validate_args(args, 1):
+        return False
+    
+    print(f'[+] Removing "{args.values[0]}" From Databases...')
+    
+    utils = DBUtils()
+    item = utils.find_item(args.values[0])
+    if not item:
+        print(f'[x] Could Not Locate "{args.values[0]}" In Databases')
+        return False
+    
+    utils.update_items_database(DatabaseUpdateType.REMOVE, item)
+    
+    print('[*] Successfully Removed', item.part_num, 'From Databases')
+    return False
+
+
+def _run_list_users(args) -> bool:
+    if not validate_args(args):
+        return False
+    _list_users()
+    return False
+
+
+def _run_search_user(args) -> bool:
+    if not validate_args(args):
+        return False
+    _run_list_users(args.values[0])
+    return False
+
+
+def _run_add_user(args) -> bool:
+    if not validate_args(args, 1):
+        return False
+    
+    print('[+] Adding User To Databases...')
+    
+    DBUtils().update_users_database(DatabaseUpdateType.ADD, args.values[0])
+    
+    print('[*] Successfully Added', args.values[0], 'To Databases')
+    return False
+
+
+def _run_remove_user(args) -> bool:
+    if not validate_args(args, 1):
+        return False
+    
+    print('[+] Removing User From Databases...')
+    
+    DBUtils().update_users_database(DatabaseUpdateType.REMOVE, args.values[0])
+    
+    print('[*] Successfully Removed', args.values[0], 'From Databases')
+    return False
+
+
+def _list_items(search_value='') -> None:
     from stock_manager import DBUtils, StockStatus
     
     print('[+]', 'Gathering Item Data...' if not search_value else f'Searching For Items With "{search_value}"...')
@@ -391,7 +337,7 @@ def _run_list_items(search_value: str = '') -> None:
     )
 
 
-def _run_list_users(search_value: str = '') -> None:
+def _list_users(search_value='') -> None:
     from stock_manager import DBUtils
     
     print('[+]', 'Gathering User Data...' if not search_value else f'Searching For Usernames With "{search_value}"...')
