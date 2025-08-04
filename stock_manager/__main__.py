3f325375dc94ca9c48749dd9b4083d5cf2d37f5f
CalciumKing <lkingerslev@gmail.com>
2025-08-04 11:44:49 -0700

    Requested #4 Changes - Commit 3
    - In __main__.py: uncommented try-except block, turned print statement into logging call, updated main() docstring.

--- stock_manager/__main__.py
+++ stock_manager/__main__.py
@@ -5,46 +5,60 @@ Initializes the Qt Application and launches the main application window.
 """
 
 import asyncio
+import logging
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from qasync import QEventLoop
 
+import stock_manager
 from stock_manager import App
 from stock_manager.cli import build_commands, entry_point
 
 
 def main() -> None:
-    """Start the Qt application and show the main window."""
+    """
+    Entry point for the stock management application.
+
+    Parses command-line arguments and determines whether to launch the GUI
+    or handle CLI-only commands.
+
+    Handles unexpected exceptions during startup, logs critical errors to
+    `app.log`, displays a fatal error dialog, and exits.
+
+    If CLI mode completes successfully or doesn't require GUI, the Qt application is never run.
+    If GUI mode is selected, initializes the Qt application and runs the main event loop.
+
+    :raise SystemExit: If a fatal error occurs during GUI startup.
+    """
+    
+    stock_manager.Logger()
     
     args = build_commands()
     
     if not entry_point(args):
         return
     
-    # try:
-    app = QApplication(sys.argv)
-    loop = QEventLoop(app)
-    asyncio.set_event_loop(loop)
-    window = App()
-    window.run()
-    window.show()
-    
-    with loop:
-        loop.run_forever()
-
-
-# except Exception as e:
-# 	from stock_manager.utils.logger import Logger
-#
-# 	print('[x] Fatal Error In Main():', e)
-# 	Logger().error_log(f'Fatal Error In Main(): {e}')
-# 	QMessageBox.critical(
-# 			None,
-# 			'Fatal Error',
-# 			'Fatal Error Starting The Application'
-# 	)
-# 	raise SystemExit(1)
+    try:
+        app = QApplication(sys.argv)
+        loop = QEventLoop(app)
+        asyncio.set_event_loop(loop)
+        window = App()
+        window.run()
+        window.show()
+        
+        with loop:
+            loop.run_forever()
+    except Exception as e:
+        from stock_manager.utils.logger import Logger
+        
+        logging.getLogger().error(f'Fatal Error In Main(): {e}')
+        QMessageBox.critical(
+                None,
+                'Fatal Error',
+                'Fatal Error Starting The Application'
+        )
+        raise SystemExit(1)
 
 
 if __name__ == '__main__':
